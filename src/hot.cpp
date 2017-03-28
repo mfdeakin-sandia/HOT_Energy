211bcab39a27da275b6577d0b85062fb30cfc956
Scott A. Mitchell <samitch@sandia.gov>
2017-03-28 13:52:39 -0600

    W1 and W2 brute force numeric integration test cases.

--- src/hot.cpp
+++ src/hot.cpp
@@ -1,7 +1,10 @@
+// hot.cpp
 
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Cartesian.h>
 #include <CGAL/number_utils.h>
+#include <CGAL/Root_of_traits.h>
 
 #include <boost/variant/variant.hpp>
 
@@ -18,7 +21,17 @@ using RNG = std::mt19937_64;
 
 using Real = float;
 
+constexpr const int dims = 2;
+
+// exact predicates, but makes debugging and printing floating point in this routine hard
 using K = CGAL::Exact_predicates_exact_constructions_kernel;
+
+// floating point predicates in the plane
+// using K = CGAL::Cartesian<Real>;
+
+// real type underlying K
+using K_real = K::RT;
+
 using DT = CGAL::Delaunay_triangulation_2<K>;
 using Face = DT::Face;
 using Point = DT::Point;
@@ -28,8 +41,6 @@ using Line = CGAL::Line_2<K>;
 using Segment = CGAL::Segment_2<K>;
 using Vector = CGAL::Vector_2<K>;
 using Direction = CGAL::Direction_2<K>;
-
-constexpr const int dims = 2;
 
 constexpr const int tri_verts = 3;
 constexpr const int tri_edges = 3;
@@ -283,7 +294,173 @@ K::RT hot_energy(const DT &dt) {
   return energy;
 }
 
+std::string pretty_print( const Segment &e )
+{
+  std::stringstream ss;
+  ss << "(" << e.source() << ")-(" << e.target() << ")";
+  return ss.str();
+}
+
+Point frac(const Segment &e, int i, int n)
+{
+  return e.source() + (i/ (double)(n-1)) * e.to_vector();
+}
+
+double wasserstein2_edge_edge(const Segment &e0, const Segment &e1)
+{
+  // std::cout << "Computing the Wasserstein distance between segements " << pretty_print(e0) << " and " << pretty_print(e1) << std::endl;
+
+  
+  // dumb brute force way
+  // each integration point of one segment is mapped to an integration point in another
+
+  // n = number of integration points,
+  // if using exact,   n <= 7 for fast, n = 8 involves 15 second wait
+  // if using Cartesian, n=8 is instant, n = 10 takes 30 seconds,
+  const int n=6;
+  assert( n > 1 );
+
+  // data for permutations
+  // numbers from 0 to n-1
+  std::vector<int> ind(n);
+  std::iota(ind.begin(), ind.end(), 0);
+  std::vector<int> best_ind(ind);
+  std::vector<int> worst_ind(ind);
+
+  // consider all possible pairings of integration points
+  // go through all the points of e0 in order
+  // go through all the points of e1 in all possible permutations
+
+  const double min_d2d = std::numeric_limits<double>::max();
+  K_real min_d2( min_d2d );
+  K_real worst_d2( 0. );
+
+  // for all permutations
+  do
+  {
+    
+    K_real d2(0.);
+    
+    // all points of e0
+    for (auto i = 0; i < n; ++i)
+    {
+      Point p0 = frac( e0, i, n );
+      Point p1 = frac( e1, ind[i], n );
+    
+      d2 += CGAL::squared_distance( p0, p1 );  // W2
+      // d2 += sqrt( CGAL::to_double( CGAL::squared_distance( p0, p1 ) ) ); // W1
+
+//      std::cout << "i: " << i << " p:" << p0 << " to j:" << ind[i] << " q:" << p1 << " d^2:" << d2 << " == " << d2_float << std::endl;
+    }
+//    std::cout << "d2:" << d2 << std::endl;
+//     std::cout << "  " << d2;
+    
+    // d2 = sum of distances^2 for this permutation
+    // best so far?
+    if ( d2 < min_d2 )
+    {
+      best_ind = ind;
+      min_d2 = d2;
+//      std::cout << "new min :" << min_d2 << std::endl;
+    }
+    if ( d2 > worst_d2 )
+    {
+      worst_ind = ind;
+      worst_d2 = d2;
+//      std::cout << "new worst :" << worst_d2 << std::endl;
+    }
+
+  } while ( std::next_permutation(ind.begin(), ind.end()) );
+  // if permutation speed becomes important, try http://howardhinnant.github.io/combinations.html
+  
+  auto min_d = sqrt( CGAL::to_double(min_d2 / n) ); // W2
+  auto worst_d = sqrt( CGAL::to_double(worst_d2 / n) ); // W2
+  // auto min_d = CGAL::to_double(min_d2) / (double) n; // W1
+  // auto worst_d = CGAL::to_double(worst_d2) / (double) n; // W1
+  
+  std::cout << "\nThe Wasserstein2 distance between segements " << pretty_print(e0) << " and " << pretty_print(e1) << " was " << min_d << std::endl;
+  std::cout << "From permutation  :";
+  for ( auto p : best_ind )
+    std::cout << " " << p;
+  std::cout << "\nWorst permutation :";
+  for ( auto p : worst_ind )
+    std::cout << " " << p;
+  std::cout << " gave distance " << worst_d << ". ";
+  
+  std::cout << std::endl << std::endl;
+
+  return min_d;
+}
+
+void wasserstein_edge_edge_test()
+{
+  // create two test edges
+  Point p0(0,0);
+  Point p1(2,0);
+  Point p2(1,0);
+  Point p3(1,2);
+  
+  // use P10--P11 for both, so distance should be zero
+  Segment e0(p0,p1);
+  Segment e1(p0,p1);
+
+  // compute distance
+//  wasserstein2_edge_edge( Segment(p0,p1), Segment(p0,p1) );
+//  wasserstein2_edge_edge( Segment(p0,p1), Segment(p1,p0) );
+  
+  // perp edges at midpoint
+  // of uniform length
+  // appears that W2 is invariant to permutation of matching up integration points!
+  // W1 order matters, best is a large spread, match close points together and match far points together
+  // W1 best is interlaced, alternating points from each side, *not* 0-5 matched with 0-5
+  if (0)
+  {
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(1,1), Point(1,3)) );      // 2.22  W2
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(1,0), Point(1,2)) );      // 1.39
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(1,-0.3), Point(1,1.7)) ); // 1.19
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(1,-0.5), Point(1,1.5)) ); // 1.09
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(1,-1), Point(1,1)) );     // 0.966
+  }
+  
+  // perp edges, not at midpoint
+  // appears that W2 is invariant to permutation of matching up integration points!
+  // W1 is as in the above case
+  if (0)
+  {
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0,1), Point(0,3)) );      // 2.43 W2
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0,0), Point(0,2)) );      // 1.71
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0,-0.3), Point(0,1.7)) ); // 1.55
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0,-0.5), Point(0,1.5)) ); // 1.47
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0,-1), Point(0,1)) );     // 1.39
+  }
+  
+  // skew
+  if (1)
+  {
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(-1,1), Point(1,1)) );
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(-1,1), Point(1,2)) );
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(-1,1), Point(1,3)) );
+
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0.2,1), Point(1.8,1)) );
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0.2,1), Point(1.8,2)) );
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0.2,1), Point(1.8,3)) );
+
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0.2,-0.3), Point(1.8,0.3)) );
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0.2,-0.3), Point(1.8,0.9)) );
+    wasserstein2_edge_edge( Segment(p0,p1), Segment( Point(0.2,-0.3), Point(1.8,1.5)) );
+  }
+}
+
 int main(int argc, char **argv) {
+  
+  // Test Wasserstein distance between two segments
+  if (0)
+  {
+    wasserstein_edge_edge_test();
+    return 0;
+  }
+  
+  // Test hot energy between triangle and triangle* == point
   DT dt;
   const int num_points = 20;
   generate_rand_dt(num_points, dt);
