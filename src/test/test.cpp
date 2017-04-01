c0a60c8899d205951ba578267b233169f76aa127
Michael Deakin <mfdeakin@cs.unc.edu>
2017-03-31 22:02:58 -0600

    Tell Catch to use it's own main function. Change the name of then 'Triangle Split' unit test to 'Right Triangle'. Split 'Right Triangle' into sections where possible

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,17 +1,32 @@
 
+#include <cmath>
+#include <limits>
+
+#include <iomanip>
 #include <iostream>
 
 #include <hot.hpp>
 
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("Triangle Split", "[HOT]") {
+TEST_CASE("Right Triangle", "[HOT]") {
+  // TODO: Reduce the large relative error of this
+  // calculation
+  // 10 bits of double precision is too much!
+  constexpr const double max_rel_error =
+      std::numeric_limits<double>::epsilon() * 1024.0;
+
   Triangle tri(Point(2, 1), Point(3, 1), Point(2, 2));
   boost::variant<std::array<Triangle, 2>,
                  std::array<Triangle, 1> >
       bounds = integral_bounds(tri);
-  REQUIRE(bounds.which() == 1);
-  auto area = boost::get<std::array<Triangle, 1> >(bounds);
-  K_real energy = triangle_w<2>(tri);
-  REQUIRE(energy == 0.5 / 9.0);
+  SECTION("Split") { REQUIRE(bounds.which() == 1); }
+  SECTION("HOT Energy") {
+    auto area =
+        boost::get<std::array<Triangle, 1> >(bounds);
+    K_real energy = triangle_w<2>(tri);
+
+    REQUIRE(std::abs(energy * 18 - 1) < 18 * max_rel_error);
+  }
 }
